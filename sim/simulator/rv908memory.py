d65efd9ee9b70ce9d28e58db0314fab433b1bfac
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-09-08 10:30:48 +0200

    Simulator: Multi-line patterns and gamma curves
    This commit mostly adds parsing of single channel gamma curves from
    the (simulated) RV908 memory. In order to do this, the hex pattern
    language needed to be extended to allow multi-line patterns. An example
    can be seen in `memory.hex`, where there are four pattern descriptors
    defined that span multiple lines (one for each of the four channels).
    
    It also became obvious that the RV908 internally uses four color
    channels (whatever the fourth one is). As a lot of parameters can be
    individually configured per channel, we introduce a data structure that
    makes it much easier to create such a parameter (represented by python).
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- sim/simulator/rv908memory.py
+++ sim/simulator/rv908memory.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import re
-from scapy.all import hexdump
 import string
-import dataclasses
-from typing import Any, Callable
+from dataclasses import dataclass, field
+from typing import Any, Callable, TypeVar, Generic
 import types
+import numpy as np
 
+T = TypeVar('T')
 
 class VerificationException(Exception):
     def __init__(self, message: str, addr: int) -> None:
@@ -27,24 +28,51 @@ class MemoryMeta(type):
         return super().__new__(cls, name, bases, namespace)
 
 
+@dataclass
+class FourChannelData(Generic[T]):
+    channel1: T
+    channel2: T
+    channel3: T
+    channel4: T
+
+    def __init__(self, factory: Callable[[], T], **kwargs) -> None:
+        self.channel1 = factory()
+        self.channel2 = factory()
+        self.channel3 = factory()
+        self.channel4 = factory()
+
+        for k,v in kwargs.items():
+            self.__setattr__(k, v)
+
+        super().__init__()
+
+    def __getitem__(self, addr):
+        return [self.channel1, self.channel2, self.channel3, self.channel4].__getitem__(addr)
+
+    def __setitem__(self, addr, value):
+        names = ["channel1", "channel2", "channel3", "channel4"][addr]
+        for name in names:
+            self.__setattr__(name, value)
+
+@dataclass
+class Parameters():
+    global_brightness: FourChannelData[int] = field(default_factory=lambda: FourChannelData(int))
+    gamma_curves: FourChannelData[list] = field(default_factory=lambda: FourChannelData(list, count=0x100))
+
 class RV908Memory(metaclass=MemoryMeta):
     _LINE_REGEX = re.compile(r"(?P<addr>[0-9a-fA-F]{4}) +(?P<data>(?:\w\w ){15}\w\w) ?(?: ; (?P<desc>(?:([A-Z]=\w+), ?)*(?:[A-Z]=\w+)))?", flags=re.ASCII)
     MEMORY_SIZE = 0x3000
-    _parsers: dict[str, Callable[[bytes], None]] = {}
-
-    @dataclasses.dataclass
-    class Parameters():
-        global_brightness: int = 0
+    _parsers: dict[str, Callable[[Any, tuple[int, int], bytes], None]] = {}
 
     def __init__(self, pattern_file: str | Path, adapt: bool = False, memory_dump_dir: None | Path = None) -> None:
 
-        self._pattern_parsers = {}
+        self._pattern_parsers: dict[tuple[int, int], Callable[[Any, tuple[int, int], bytes], None]] = {}
         self._pattern_arr = bytearray(self.MEMORY_SIZE)
 
         self._load_pattern(Path(pattern_file))
 
         self._memory = bytearray(self._pattern_arr)
-        self.params = RV908Memory.Parameters()
+        self.params = Parameters()
         self._should_adapt = adapt
         self._memory_dump_dir = memory_dump_dir
 
@@ -52,6 +80,8 @@ class RV908Memory(metaclass=MemoryMeta):
         with open(file, "r") as fp:
             fdata = fp.read()
 
+        pattern_loc = {}
+        _current_pattern = None
         for idx, line in enumerate(fdata.splitlines(), start=1):
             if line.strip().startswith("#"):
                 # ignore comment
@@ -69,25 +99,28 @@ class RV908Memory(metaclass=MemoryMeta):
                 desc = {(v := elem.strip().split("="))[0]: v[1] for elem in p.group("desc").split(",")}
 
             # find and replace data substitutions
-            pattern_loc = {}
-            _cur_pat = None
             _line_data = b""
             for pos, elem in enumerate(data.split()):
                 if elem[0] not in string.ascii_uppercase and elem[1] not in string.ascii_uppercase:
                     _line_data += bytes.fromhex(elem)
+                    _current_pattern = None
                 elif (v := elem[0]) == elem[1]:
                     start, end = addr + pos, addr + pos + 1
-                    if _cur_pat == v:
+
+                    if _current_pattern == v:
                         start = pattern_loc[v][0]
+                    elif v in pattern_loc:
+                        raise Exception(f"line {idx}: Reuse of pattern {v} in same line")
 
                     pattern_loc[v] = (start, end)
 
-                    _cur_pat = v
+                    _current_pattern = v
                     _line_data += b"\x00"
                 else:
                     raise Exception(f"line {idx}: Unable to parse byte {elem} - should be lower case hex or two same upper case letters for a pattern")
 
-            if set(pattern_loc) != set(desc):
+            # check that all descriptions are there (except current active one)
+            if set(pattern_loc) - {_current_pattern} != set(desc) - {_current_pattern}:
                 raise Exception(f"line {idx}: Not all descriptions are mentioned in the hex or vice versa (data: {set(pattern_loc)} | desc: {set(desc)})")
 
             # add line data to pattern bytes
@@ -100,9 +133,14 @@ class RV908Memory(metaclass=MemoryMeta):
                     raise Exception(f"line {idx}: Unknown description parser `{v}`")
                 self._pattern_parsers[pattern_loc[k]] = _parser
 
+                # clear temporaries
+                del pattern_loc[k]  # will clear pattern_loc except for _current_pattern if that is not in desc
+                if _current_pattern == k:  # clear current_pattern, if there is a desc. def. for it here
+                    _current_pattern = None
+
     def parse(self):
         for (s, e), f in self._pattern_parsers.items():
-            f(self, self._memory[s:e])
+            f(self, (s, e), self._memory[s:e])
 
     def verify_memory(self, start: None | int = None, end: None | int = None):
         def in_some_range(idx: int) -> bool:
@@ -157,5 +195,22 @@ class RV908Memory(metaclass=MemoryMeta):
     #############################################
 
     @parameter_parser
-    def global_brightness(self, v: bytes):
-        self.params.global_brightness = v[0]
+    def global_brightness(self, _: tuple[int, int], v: bytes):
+        for i, brightness in enumerate(v):
+            self.params.global_brightness[i] = brightness
+
+    @parameter_parser
+    def gamma_curve(self, addr: tuple[int, int], v: bytes):
+        # get channel by looking at the address of the data
+        i = [0x2000, 0x2800, 0x2A00, 0x2C00].index(addr[0])
+        if i == -1:
+            raise IndexError(f"unknown starting address area for gamma {addr[0]} ({addr})")
+
+        # parse the data
+        lookup = np.frombuffer(v, dtype=np.uint16)
+
+        # verify data
+        if len(lookup) != self.params.gamma_curves.count:
+            raise Exception(f"Invalid size. Expected {self.params.gamma_curves.count}, got {len(lookup)}")
+
+        self.params.gamma_curves[i] = list(lookup)
