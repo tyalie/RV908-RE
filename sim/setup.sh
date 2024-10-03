65ea04d657d67ccf9faf14a2bcf699a689001ecf
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-10-03 23:58:20 +0200

    [feat] sim: Adapt setup.sh for bridge topology
    Connecting each endpoint using a bridge allows us to connect more than
    two devices to the same package stream. With this the simulator can
    listen to the traffic between LEDSet and real hardware without requiring
    additional permissions.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- sim/setup.sh
+++ sim/setup.sh
@@ -8,19 +8,35 @@ if [[ -n "$SUDO_USER" ]]; then
     user="$SUDO_USER"
 fi
 
+br_name="br-led"
 link_name="rv908simtun0"
 
 if [[ "$1" == "up" ]]; then
-  echo "Starting $link_name"
+  echo "Creating bridge $br_name"
+  ip link add name "$br_name" type bridge
+  ip link set dev "$br_name" up
+
+  echo "Starting tap $link_name"
 
   # using tap here as we also wanna emulate the ethernet headers
   ip tuntap add name "$link_name" mode tap user $user
+  ip link set dev "$link_name" master "$br_name"
+  ip link set dev "$link_name" qlen 10000
   ip link set "$link_name" up
+
 elif [[ "$1" == "down" ]]; then
   echo "Stopping $link_name"
 
   ip link set "$link_name" down
   ip tuntap del name "$link_name" mode tap
+
+  echo "Removing bridge $br_name"
+  ip link set "$br_name" down
+  ip link del name "$br_name"
+elif [[ "$1" == "add-if" ]]; then
+  if_name="$2"
+  echo "Adding '$if_name' to '$br_name'"
+  ip link set dev "$if_name" master "$br_name"
 else
   echo "Error. Unknown command '$1' (must be up or down)"
 fi
