a6d20e69a56b520debbf9c788675f6266dbc6327
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-10-03 23:24:33 +0200

    [feat] sim: Sparse memory storage and 3 byte addr.
    The RV908 has multiple memory regions corresponding to the respective
    memory types (e.g. volatile, non-volatile, …), but the overall data
    layout does not change between them. This became clear after discovering
    that the addressing is done with three bytes, instead of two.
    
    With the sparse memory we can only store the memory areas that are
    relevant and have two memory regions with identical patterns share one
    pattern definition.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- sim/simulator/rv908memory.py
+++ sim/simulator/rv908memory.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import re
+import copy
 import string
 from dataclasses import dataclass, field
-from typing import Any, Callable, TypeVar, Generic
+import itertools
+from typing import Any, Callable, TypeVar, Generic, MutableSequence, Iterator
 import types
 import numpy as np
 
@@ -54,27 +56,121 @@ class FourChannelData(Generic[T]):
         for name in names:
             self.__setattr__(name, value)
 
+class SparseMemory(Generic[T]):
+    """
+    the linsn receivers have volatile and non-volatile memory.
+    This is used quite often by Linsn for their temporary and
+    permentant testing and deploying of configuration data.
+    Presumably to simplify the loading portion, they only change
+    the highest byte which means that the data layout and addr (& 0xFFFF)
+    stay the same. Using a mapping between these areas we can spot diffs
+    and keep our memory.hex in sync.
+    """
+
+    def __init__(self, *ignore, memory: dict[tuple[int, int], MutableSequence[T]] = {}) -> None:
+        if ignore:
+            raise TypeError
+
+        self._memory: dict[tuple[int, int], MutableSequence[T]] = memory
+
+    @classmethod
+    def from_layout(cls, layout: dict[tuple[int, int], str], init_func: Callable[[int], MutableSequence[T]]):
+        inst = cls()
+
+        def intersect(r1, r2):
+            return max(r1[0],r2[0]) <= min(r1[1],r2[1])
+
+        for x,y in itertools.combinations(layout, 2):
+            if intersect(x, y):
+                raise ValueError(f"Ranges {x} and {y} are overlapping")
+
+        _tmp = {}
+        for r, name in layout.items():
+            _len = r[1] - r[0]
+            if _len < 0:
+                raise Exception(f"Size should be larger than 0 bytes (got: {_len})")
+            inst._memory[r] = _tmp.setdefault(name, init_func(_len))
+        return inst
+
+    @property
+    def ranges(self) -> list[tuple[int, int]]:
+        return sorted(self._memory, key=lambda v: v[1])
+
+    def idx_iter(self) -> Iterator[int]:
+        for s, e in self.ranges:
+            yield from range(s, e)
+
+    def _getsubarray(self, addr: int | slice) -> MutableSequence[T]:
+        if isinstance(addr, slice):
+            _start, _end = addr.start, addr.stop
+        else:
+            _start, _end = addr, addr
+
+        if _end < _start:
+            _start, _end = _end, _start
+
+        for r in self.ranges:
+            if r[0] <= _start and _end <= r[1]:
+                return self._memory[r]
+
+        raise IndexError(f"Couldn't find continous memory segment for slice({_start:06X}, {_end:06X})")
+
+    def __getitem__(self, addr):
+        return self._getsubarray(addr)[addr]
+
+    def __setitem__(self, addr, value):
+        _l = self._getsubarray(addr)
+        if isinstance(addr, slice) and len(_l[addr]) != len(value):
+            raise ValueError(f"Size mismatch between size and given slice")
+        _l[addr] = value
+
+    def __copy__(self):
+        import copy
+
+        n_memory, _tmp = {}, {}
+        for r, v in self._memory.items():
+            n_memory[r] = _tmp.setdefault(id(v), copy.copy(v))
+        return self.__class__(memory=n_memory)
+
+    def non_sparse(self, empty_init: Callable[[int], MutableSequence[T]]) -> MutableSequence[T]:
+        # empty output
+        out = empty_init(0)
+
+        cur_idx = 0
+        breakpoint()
+        for r in self.ranges:
+            if cur_idx < r[0]:
+                out += empty_init(r[0] - cur_idx)
+
+            cur_idx = r[1]
+            out += self._memory[r]
+
+        return out
+
 @dataclass
 class Parameters():
     global_brightness: FourChannelData[int] = field(default_factory=lambda: FourChannelData(int))
     gamma_curves: FourChannelData[list] = field(default_factory=lambda: FourChannelData(list, count=0x100))
 
 class RV908Memory(metaclass=MemoryMeta):
-    _LINE_REGEX = re.compile(r"(?P<addr>[0-9a-fA-F]{4}) +(?P<data>(?:\w\w ){15}\w\w) ?(?: ; (?P<desc>(?:([A-Z]=\w+), ?)*(?:[A-Z]=\w+)))?", flags=re.ASCII)
-    MEMORY_SIZE = 0x3000
+    _LINE_REGEX = re.compile(r"(?P<addr>[0-9a-fA-F]{6}) +(?P<data>(?:\w\w ){15}\w\w) ?(?: ; (?P<desc>(?:([A-Z]=\w+), ?)*(?:[A-Z]=\w+)))?", flags=re.ASCII)
     _parsers: dict[str, Callable[[Any, tuple[int, int], bytes], None]] = {}
 
     def __init__(self, pattern_file: str | Path, adapt: bool = False, memory_dump_dir: None | Path = None) -> None:
 
         self._pattern_parsers: dict[tuple[int, int], Callable[[Any, tuple[int, int], bytes], None]] = {}
-        self._pattern_arr = bytearray(self.MEMORY_SIZE)
+        self._memory_pat = SparseMemory.from_layout({
+            (0x000000, 0x002F00): "display config",
+        }, bytearray)
+        self._memory_mask = copy.copy(self._memory_pat)
 
         self._load_pattern(Path(pattern_file))
 
-        self._memory = bytearray(self._pattern_arr)
-        self.params = Parameters()
+        self._memory = copy.copy(self._memory_pat)
         self._should_adapt = adapt
         self._memory_dump_dir = memory_dump_dir
+
+        self.params = Parameters()
 
     def _load_pattern(self, file: Path):
         with open(file, "r") as fp:
@@ -83,8 +179,8 @@ class RV908Memory(metaclass=MemoryMeta):
         pattern_loc = {}
         _current_pattern = None
         for idx, line in enumerate(fdata.splitlines(), start=1):
-            if line.strip().startswith("#"):
-                # ignore comment
+            if line.strip().startswith("#") or len(line.strip()) == 0:
+                # ignore comment or empty lines (used as visual markers)
                 continue
 
             p = RV908Memory._LINE_REGEX.fullmatch(line)
@@ -124,14 +220,17 @@ class RV908Memory(metaclass=MemoryMeta):
                 raise Exception(f"line {idx}: Not all descriptions are mentioned in the hex or vice versa (data: {set(pattern_loc)} | desc: {set(desc)})")
 
             # add line data to pattern bytes
-            self._pattern_arr[addr:addr + 16] = _line_data
+            self._memory_pat[addr:addr + 16] = _line_data
 
             # add to global area -> description parser dictionary
             for k, v in desc.items():
                 _parser = self._parsers.get(v)
                 if _parser is None:
                     raise Exception(f"line {idx}: Unknown description parser `{v}`")
-                self._pattern_parsers[pattern_loc[k]] = _parser
+
+                _loc = pattern_loc[k]
+                self._pattern_parsers[_loc] = _parser
+                self._memory_mask[_loc[0]:_loc[1]] = b'\x01' * (_loc[1] - _loc[0])
 
                 # clear temporaries
                 del pattern_loc[k]  # will clear pattern_loc except for _current_pattern if that is not in desc
@@ -142,21 +241,10 @@ class RV908Memory(metaclass=MemoryMeta):
         for (s, e), f in self._pattern_parsers.items():
             f(self, (s, e), self._memory[s:e])
 
-    def verify_memory(self, start: None | int = None, end: None | int = None):
-        def in_some_range(idx: int) -> bool:
-            for (s, e) in self._pattern_parsers:
-                if s <= idx and idx < e:
-                    return True
-            return False
-
-        assert len(self._memory) == len(self._pattern_arr), "Mismatching size between pattern and memory"
-
-        start = 0 if start is None else start
-        end = len(self._memory) if end is None else end
-
+    def verify_memory(self, start: int, end: int):
         for idx in range(start, end):
-            if self._memory[idx] != self._pattern_arr[idx] and not in_some_range(idx):
-                raise VerificationException(f"Verification failed - mismatch at addr 0x{idx:04X} (excepted: 0x{self._pattern_arr[idx]:02X} | got: 0x{self._memory[idx]:02X})", idx)
+            if self._memory[idx] != self._memory_pat[idx] and not self._memory_mask[idx]:
+                raise VerificationException(f"Verification failed - mismatch at addr 0x{idx:04X} (excepted: 0x{self._memory_pat[idx]:02X} | got: 0x{self._memory[idx]:02X})", idx)
 
     def __getitem__(self, addr):
         return self._memory.__getitem__(addr)
@@ -171,7 +259,7 @@ class RV908Memory(metaclass=MemoryMeta):
                 self.verify_memory(addr, addr + 1)
         finally:
             if self._should_adapt:
-                self._pattern_arr[addr] = value
+                self._memory_pat[addr] = value
 
     def store_dump(self):
         if self._memory_dump_dir is None or not self._memory_dump_dir.is_dir():
@@ -186,7 +274,7 @@ class RV908Memory(metaclass=MemoryMeta):
         # write to next free idx
         name = f"{max_idx + 1:04}.bin"
         with (self._memory_dump_dir / name).open("wb") as fp:
-            fp.write(self._memory)
+            fp.write(self._memory.non_sparse(bytearray))
 
         print(f"-- Written memory dump to {name}")
 
