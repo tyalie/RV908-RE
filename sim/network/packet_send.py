4473ab6bc02c0cffca79cca0a02493d998304319
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-10-03 23:37:19 +0200

    [feat] sim: Correct CmdsConfigData definition
    Analysis of the config packages turned up, that the previous definitions
    of CmdsConfigData were incorrect / incomplete. For one: The delimiter is
    bollocks, it is the highest byte of the three byte address - instead of
    some random stuff.
    
    Also, the flag is presumably just an enum. This makes the parsing /
    verifying and such much easier in the long run. Interpreting it as
    flags, did not make sense with the introduction of volatile safes.
    
    Using this the verification of the CmdsConfigData has been overhauled
    completely and tested.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- sim/network/packet_send.py
+++ sim/network/packet_send.py
@@ -1,13 +1,14 @@
 from scapy.all import *
 from itertools import accumulate
 
+from enum import Enum, unique
 from network.custom_types import BytesEnum
 from network.custom_fields import VerifiablePacket
 
 class LinsnCMDs(BytesEnum):
-    MACADDR  = bytes.fromhex('000000000000960000')
-    CONFIG   = bytes.fromhex("00000000000061ff00")
-    NULL     = bytes.fromhex("000000000000000000")
+    MACADDR  = bytes.fromhex('00000000000096')
+    CONFIG   = bytes.fromhex("00000000000061")
+    NULL     = bytes.fromhex("00000000000000")
 
 
 class LinsnLEDSend(VerifiablePacket):
@@ -15,7 +16,8 @@ class LinsnLEDSend(VerifiablePacket):
     fields_desc = [
         # header fields (32 bytes)
         LEShortField("pkg_count", None),
-        XStrFixedLenField("cmd", None, 9),
+        XStrFixedLenField("cmd", None, 7),
+        ShortField("panel_idx", None),
         PacketLenField("cmd_data", None, lambda payload, _parent: LinsnLEDSend.guess_cmd_data_params_class(_parent, payload), lambda _: 20),
         XByteField("chksum", None),  # ?
 
@@ -33,15 +35,30 @@ class LinsnLEDSend(VerifiablePacket):
                 return self.CmdNull(payload)
 
     class CmdsConfigData(Packet):
+        @unique
+        class TypeEnum(Enum):
+            TMP_DATA  = 0x1
+            CONF      = 0x5
+            PERM_DATA = 0x8
+            PERM_CONF = 0x9
+
+            def short_name(self):
+                return "".join(map(lambda s: s[0].upper(), self.name.split("_")))
+
         fields_desc = [
-            FlagsField("flag", None, 3, {0x2: "cmd_bounds"}),  # ?
-            XBitField("ones", 0b1, 1),  # ?
-            FlagsField("flag2", None, 1, {0x1: "is_data"}),  # ?
+            BitEnumField("type", None, 4, TypeEnum),
+            BitField("is_data", None, 1),
             BitField("idx", None, 3),
-            XByteField("deli", None),
-            XShortField("address", None),  # ?
+            X3BytesField("address", None),
             XStrFixedLenField("data", None, 16),
         ]
+
+        @property
+        def type_e(self) -> None | TypeEnum:
+            try:
+                return self.TypeEnum(self.type)
+            except ValueError:
+                return None
 
     class CmdMacAddrData(Packet):
         fields_desc = [
@@ -76,6 +93,10 @@ class LinsnLEDSend(VerifiablePacket):
         return self.cmd in LinsnCMDs
 
     @VerifiablePacket.is_test
+    def is_panel_idx_right(self):
+        return self.cmd == LinsnCMDs.CONFIG or self.panel_idx == 0
+
+    @VerifiablePacket.is_test
     def enforce_defaults(self):
         # to make it easier for us: instead of taking defaults as a recommendation,
         # we will try to enforce it here. This makes it easy to declare constants
@@ -96,16 +117,41 @@ class LinsnLEDSend(VerifiablePacket):
         if not isinstance(self.cmd_data, self.CmdsConfigData):
             return True
 
-        # check if any unknown flags
-        _unknown_flags = not any("bit_" in v for v in str(self.cmd_data.flag).split("+"))
-        _is_bounds = str(self.cmd_data.flag) == "cmd_bounds"
+        """ The memory transfer protocol
 
-        # check flag2
-        _right_flag2 = self.cmd_data.flag2 != _is_bounds
-        # check if ones are ones
-        _are_ones_ones = self.cmd_data.ones == 0b1
-        # check if delimiter in 00 or 0xaa
-        _right_deli = self.cmd_data.deli == (0xaa if _is_bounds else 0x00)
-        return _unknown_flags and _are_ones_ones and _right_deli and _right_flag2
+        I EEE D| AAAAAA  SS rest-of-the-data …
+        I: Idx [0-7]/ E: package type enum /
+        D: Data / A: Address / S: Starting byte [part of the data]
+
+        D is a bit flags, I, E and A are integers
+        """
+
+        is_correct = True
+        match self.CmdsConfigData.type_e:
+            case self.CmdsConfigData.TypeEnum.CONF:
+                # transfer start or end
+                is_correct &= self.cmd_data.is_data == 0
+                is_correct &= self.cmd_data.address == 0xAA55AA
+                is_correct &= self.cmd_data.data[0] in [0x55, 0x99, 0x0]
+                is_correct &= self.cmd_data.data[1:] == b"\xd8" * 15
+            case self.CmdsConfigData.TypeEnum.PERM_CONF:
+                # config used for permanent sequences (currently only erase is known)
+                is_correct &= self.cmd_data.is_data == 0
+                # erase has only been observed with 4kiB aligned step size
+                is_correct &= (self.cmd_data.address % 0x1000) == 0
+                # this presumably means erase
+                is_correct &= self.cmd_data.data[0:2] == b"\x00\x00"
+                is_correct &= self.cmd_data.data[2:] == b"\xd8" * 14
+            case self.CmdsConfigData.TypeEnum.TMP_DATA:
+                # temporary data
+                is_correct &= self.cmd_data.is_data == 1
+            case self.CmdsConfigData.TypeEnum.PERM_DATA:
+                # permanent data
+                is_correct &= self.cmd_data.is_data == 1
+            case _:
+                is_correct = False
+
+        return is_correct
+
 
 bind_layers(Ether, LinsnLEDSend, type=0xaa55)
